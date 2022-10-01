22264d12decf1c754f3e3c352fa456c795637b65
P-Ajith <114827943+P-Ajith@users.noreply.github.com>
2022-10-01 23:23:35 +0530

    Add files via upload

---
+++ 01_Day_Introduction/datatypes.js
@@ -0,0 +1,37 @@
+
+let name1 = "alex";
+let count1 = 5;
+let start;
+let empty = null;
+
+console.log(typeof(name1));
+console.log(typeof count1);
+console.log(typeof(start));
+console.log(typeof null);
+
+let firstvalue;
+let secondvalue;
+let thirdvalue;
+let fourthvalue;
+
+let value1 = 1;
+let value2 = "two";
+let value3 = 3;
+let value4 = "four";
+
+let firstName2 = "alex";
+let lastName2 = "a";
+let isMarried2 = true;
+let country2 = "india";
+let age2 = 25;
+console.log(firstName2, lastName2, isMarried2, country2, age2);
+
+let firstName3 = "allen", lastName3 = "a", isMarried3 = true, country3 = "india", age3 = 25;
+
+console.log(firstName3, lastName3, isMarried3, country3, age3);
+
+let myAge = 27;
+let yourAge = 23;
+
+console.log("i am", myAge, "years old." );
+console.log("you are", yourAge, "years old");
---
+++ 01_Day_Introduction/variable.js
@@ -0,0 +1,12 @@
+ //comments can make code readable
+    //welcome to 30DaysOfJavaScript
+
+    /* comments can make code readable, easy to reuse and informative */
+
+let count = "starting";
+let isStarted = true;
+let song;
+let city = null;
+
+console.log(count, isStarted, song, city);
+
